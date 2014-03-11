06268d80bba235cc24b546c097ca5ce1129d70a0
Michael Montero <mcmontero@gmail.com>
2014-03-11 16:38:42 -0400

    The reference definition file is now automatically loaded if it is defined.

--- __init__.py
+++ __init__.py
@@ -1,8 +1,17 @@
 # ----- Imports --------------------------------------------------------------
 
 from .dsh import dsh
+from importlib.machinery import SourceFileLoader
+from tinyAPI.base.config import ConfigManager
 from tinyAPI.base.data_store.memcache import Memcache
 from tinyAPI.base.data_store.exception import DataStoreDuplicateKeyException
 from tinyAPI.base.data_store.provider import DataStoreMySQL
 from tinyAPI.base.services.table_builder.mysql import Table
 from tinyAPI.base.services.table_builder.mysql import RefTable
+
+# ----- Instructions ---------------------------------------------------------
+
+ref_defs_file = ConfigManager.value('reference definition file')
+if ref_defs_file is not None:
+    loader = SourceFileLoader('reference_definition', ref_defs_file)
+    loader.load_module('reference_definition')

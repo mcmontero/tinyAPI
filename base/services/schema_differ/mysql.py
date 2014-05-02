8f31b344e01e06af989c47a30958aaee28e3379b
Michael Montero <mcmontero@gmail.com>
2014-05-02 16:05:37 +0000

    Initial logic around computing differences is complete.

--- base/services/schema_differ/mysql.py
+++ base/services/schema_differ/mysql.py
@@ -6,6 +6,8 @@ __author__ = 'Michael Montero <mcmontero@gmail.com>'
 
 from tinyAPI.base.data_store.provider import DataStoreMySQL
 
+import re
+
 __all__ = [
     'SchemaDiffer'
 ]
@@ -25,7 +27,6 @@ class SchemaDiffer(object):
         self.__target = None
         self.__source_db_name = None
         self.__target_db_name = None
-        self.__write_upgrade_scripts = None
         self.__ref_tables_to_create = None
         self.__ref_tables_to_drop = None
         self.__tables_to_create = None
@@ -54,7 +55,7 @@ class SchemaDiffer(object):
         self.__target.select_db(target_connection_name, 'information_schema')
         self.__target_db_name = target_db_name
 
-        self.__write_upgrade_scripts = True
+        self.__enable_write_upgrade_scripts = True
 
 
     def __compute_column_differences(self):
@@ -287,6 +288,54 @@ class SchemaDiffer(object):
                 .insert(index['seq_in_index'], index['column_name'])
 
 
+        indexes_to_create = \
+            list(set(source_names).difference(target_names))
+        indexes_to_drop = \
+            list(set(target_names).difference(source_names))
+        indexes_to_modify = \
+            []
+
+        for name, data in source.items():
+            if name in target.keys() and \
+               ','.join(data['cols']) != ','.join(target[name]['cols']):
+                indexes_to_modify.append(name)
+
+        self.__indexes_to_create = []
+        for name in indexes_to_create:
+            self.__notice('(+) ' + name, 1)
+
+            self.__indexes_to_create.append({
+                'table_name': source[name]['table_name'],
+                'index_name': name,
+                'cols': source[name]['cols']
+            })
+
+        self.__indexes_to_drop = []
+        for name in indexes_to_drop:
+            self.__notice('(-) ' + name, 1)
+
+            self.__indexes_to_drop.append({
+                'table_name': target[name]['table_name'],
+                'index_name': name,
+                'cols': target[name]['cols']
+            })
+
+        for name in indexes_to_modify:
+            self.__notice('(=) ' + name, 1)
+
+            self.__indexes_to_create.append({
+                'table_name': source[name]['table_name'],
+                'index_name': name,
+                'cols': source[name]['cols']
+            })
+
+            self.__indexes_to_drop.append({
+                'table_name': target[name]['table_name'],
+                'index_name': name,
+                'cols': target[name]['cols']
+            })
+
+
     def __compute_ref_table_data_differences(self):
         self.__notice('Computing reference table data differences...')
 
@@ -458,8 +507,67 @@ class SchemaDiffer(object):
         self.__table_create_drop_list = ','.join(create_drop_list)
 
 
+    def __compute_unique_key_differences(self):
+        self.__notice('Computing unique key differences...')
+
+        query = \
+            """select table_name,
+                      constraint_name,
+                      column_name,
+                      ordinal_position
+                 from key_column_usage
+                where table_schema = %s
+                  and constraint_name like '%\_uk'"""
+        if self.__table_create_drop_list:
+            query += ' and table_name not in (' \
+                     + self.__table_create_drop_list \
+                     + ')'
+
+        source_uks = \
+            self.__process_uks(
+                self.__query_source(
+                    query,
+                    [self.__source_db_name]))
+        target_uks = \
+            self.__process_uks(
+                self.__query_target(
+                    query,
+                    [self.__target_db_name]))
+
+        source_uk_names = source_uks.keys()
+        target_uk_names = target_uks.keys()
+
+        unique_keys_to_create = \
+            list(set(source_uk_names).difference(target_uk_names))
+        unique_keys_to_drop = \
+            list(set(target_uk_names).difference(source_uk_names))
+
+        self.__unique_keys_to_create = []
+        for name in unique_keys_to_create:
+            self.__notice('(+) ' + name, 1)
+            self.__unique_keys_to_create.append(source_uks[name])
+
+        self.__unique_keys_to_drop = []
+        for name in unique_keys_to_drop:
+            self.__notice('(-) ' + name, 1)
+            self.__unique_keys_to_drop.append(target_uks[name])
+
+        for name, uk in source_uks.items():
+            if name in target_uks.keys() and \
+               name not in unique_keys_to_create and \
+               name not in unique_keys_to_drop:
+                if source_uks[name]['table_name'] != \
+                   target_uks[name]['table_name'] or \
+                   ','.join(source_uks[name]['cols'].values()) != \
+                   ','.join(target_uks[name]['cols'].values()):
+                    self.__notice('(=) ' + name, 1)
+
+                    self.__unique_keys_to_drop.append(source_uks[name])
+                    self.__unique_keys_to_create.append(source_uks[name])
+
+
     def dont_write_upgrade_scripts(self):
-        self.__write_upgrade_scripts = False
+        self.__enable_write_upgrade_scripts = False
         return self
 
 
@@ -479,6 +587,13 @@ class SchemaDiffer(object):
         self.__compute_foreign_key_differences()
         self.__compute_ref_table_data_differences()
         self.__compute_index_differences()
+        self.__compute_unique_key_differences()
+
+        if not self.__there_are_differences():
+            self.__notice('Both schemas are the same!')
+            exit(0)
+
+        self.__write_upgrade_scripts()
 
         self.__target.close()
         self.__source.close()
@@ -495,12 +610,41 @@ class SchemaDiffer(object):
         return results
 
 
+    def __get_column_terms(self, column_data):
+        terms = []
+
+        if column_data['extra'] == 'auto_increment':
+            terms.append('auto_increment')
+
+        if column_data['column_key'] == 'UNI':
+            terms.append('unique')
+
+        if column_data['character_set_name']:
+            terms.append('character set ' + column_data['character_set_name'])
+
+        if column_data['collation_name']:
+            terms.append('collate ' + column_data['collation_name'])
+
+        if column_data['column_default']:
+            terms.append('default '
+                         + ('current_timestamp'
+                                if column_data['column_default'] ==
+                                   'current_timestamp'
+                                else "'" + column_data['column_default'] + "'"))
+
+        if column_data['is_nullable'] == 'NO':
+            terms.append('not null')
+
+        return terms
+
+
     def __ksort(self, data):
         results = {}
         for key in sorted(data.keys()):
             results[key] = data[key]
 
         return results
+
 
     def __notice(self, message, indent=None):
         if not self.__cli:
@@ -543,6 +687,27 @@ class SchemaDiffer(object):
         return fks
 
 
+    def __process_uks(self, data=tuple()):
+        uks = {}
+        for uk in data:
+            if uk['constraint_name'] not in uks.keys():
+                uks[uk['constraint_name']] = {
+                    'name': uk['constraint_name'],
+                    'table_name': uk['table_name'],
+                    'cols': {}
+                }
+
+            uks[uk['constraint_name']] \
+               ['cols'] \
+               [int(uk['ordinal_position'])] = \
+                uk['column_name']
+
+        for name, uk in uks.items():
+            uks[name]['cols'] = self.__ksort(uks[name]['cols'])
+
+        return uks
+
+
     def __query_source(self, query, binds=tuple()):
         return self.__source.query(query, binds)
 
@@ -554,6 +719,26 @@ class SchemaDiffer(object):
     def set_cli(self, cli):
         self.__cli = cli
         return self
+
+
+    def __there_are_differences(self):
+        return self.__ref_tables_to_create or \
+               self.__ref_tables_to_drop or \
+               self.__tables_to_create or \
+               self.__tables_to_drop or \
+               self.__columns_to_create or \
+               self.__columns_to_drop or \
+               self.__columns_to_modify or \
+               self.__column_uniqueness_to_drop or \
+               self.__foreign_keys_to_create or \
+               self.__foreign_keys_to_drop or \
+               self.__ref_data_to_add or \
+               self.__ref_data_to_remove or \
+               self.__ref_data_to_modify or \
+               self.__indexes_to_create or \
+               self.__indexes_to_drop or \
+               self.__unique_keys_to_create or \
+               self.__unique_keys_to_drop
 
 
     def __verify_schemas(self):
@@ -579,3 +764,277 @@ class SchemaDiffer(object):
                          + '" does not exist',
                          1)
             exit(1)
+
+
+    def __write_add_foreign_key_constraint_sql(self):
+        file_name = '55-foreign_keys.sql'
+
+        self.__notice(file_name, 1)
+
+        contents = ''
+        for fk in self.__foreign_keys_to_create:
+            contents += \
+                ('alter table ' + fk['table_name'] + '\n'
+              +  '        add constraint ' + fk['name'] + '\n'
+              +  '    foreign key (' + ', '.join(fk['cols'].values()) + ')\n'
+              +  ' references ' + fk['ref_table_name'] + '\n'
+              +  '            (' + ', '.join(fk['ref_cols'].values()) + ')\n'
+              +  '  on delete ' + fk['delete_rule'].lower() + ';\n\n')
+
+        self.__write_file(file_name, contents)
+
+
+    def __write_add_indexes_sql(self):
+        file_name = '65-indexes.sql'
+
+        self.__notice(file_name, 1)
+
+        contents = ''
+        for index in self.__indexes_to_create:
+            contents += 'create index ' + index['index_name'] + '\n' \
+                      + '          on ' + index['table_name'] + '\n' \
+                      + '             (' + ', '.join(index['cols']) + ');\n\n'
+
+        self.__write_file(file_name, contents)
+
+
+    def __write_add_modify_columns_sql(self):
+        file_name = '35-columns.sql'
+
+        self.__notice(file_name, 1)
+
+        contents = ''
+        for name in self.__columns_to_create:
+            table_name, column_name = name.split('.')
+
+            column = self.__source.query(
+                '''select table_name,
+                          column_name,
+                          column_default,
+                          is_nullable,
+                          character_set_name,
+                          collation_name,
+                          column_type,
+                          column_key,
+                          extra
+                     from information_schema.columns
+                    where table_name = %s
+                      and column_name = %s''',
+                [table_name, column_name])
+
+            contents += 'alter table ' + column[0]['table_name'] + "\n" \
+                      + '        add ' + column[0]['column_name'] + "\n" \
+                      + '            ' + column[0]['column_type']
+
+            terms = self.__get_column_terms(column[0])
+            if terms:
+                contents += "\n"
+
+            for index in range(len(terms)):
+                terms[index] = "            " + terms[index]
+            contents += "\n".join(terms) + ";\n\n"
+
+        for column in self.__columns_to_modify.values():
+            contents += 'alter table ' + column['table_name'] + "\n" \
+                      + '     modify ' + column['column_name'] + "\n" \
+                      + '            ' + column['column_type']
+
+            terms = self.__get_column_terms(column)
+            if terms:
+                contents += "\n"
+
+            for index in range(len(terms)):
+                terms[index] = "            " + terms[index]
+            contents += "\n".join(terms) + ";\n\n"
+
+        for column in self.__columns_to_drop:
+            table_name, column_name = column.split('.')
+
+            contents += 'alter table ' + table_name + "\n" \
+                      + '       drop ' + column_name + ";\n\n"
+
+        for column in self.__column_uniqueness_to_drop:
+            table_name, column_name = column.split('.')
+
+            index = self.__target.query(
+                '''show index
+                   from ''' + self.__target_db_name + "." + table_name + '''
+                   where column_name = %s''',
+                [column_name])
+
+            contents += 'alter table ' + index[0]['Table'] + "\n" \
+                      + '       drop index ' + index[0]['Key_name'] + ";\n\n"
+
+        self.__write_file(file_name, contents)
+
+
+    def __write_add_ref_tables_sql(self):
+        file_name = '10-ref_tables.sql'
+
+        self.__notice(file_name, 1)
+
+        contents = ''
+        for name in self.__ref_tables_to_create:
+            record = self.__source.query(
+                'show create table ' + self.__source_db_name + '.' + name)
+            contents += record[0]['Create Table'] + ";\n\n"
+
+        if contents:
+            contents += "\n"
+
+        self.__write_file(file_name, contents)
+
+
+    def __write_add_tables_sql(self):
+        file_name = '30-tables.sql'
+
+        self.__notice(file_name, 1)
+
+        contents = ''
+        for name in self.__tables_to_create:
+            record = self.__source.query(
+                'show create table ' + self.__source_db_name + '.' + name)
+
+            contents += record[0]['Create Table'] + ";\n\n\n"
+
+        self.__write_file(file_name, contents)
+
+
+    def __write_add_unique_key_constraint_sql(self):
+        file_name = '60-unique_keys.sql'
+
+        self.__notice(file_name, 1)
+
+        contents = ''
+        for uk in self.__unique_keys_to_create:
+            contents += \
+                'alter table ' + uk['table_name'] + '\n' \
+              + '        add unique key ' + uk['name'] + '\n' \
+              + '            (' + ', '.join(uk['cols'].values()) + ');\n\n'
+
+        self.__write_file(file_name, contents)
+
+
+    def __write_drop_foreign_key_constraint_sql(self):
+        file_name = '15-foreign_keys.sql'
+
+        self.__notice(file_name, 1)
+
+        contents = ''
+        for fk in self.__foreign_keys_to_drop:
+            contents += 'alter table ' + fk['table_name'] + "\n" \
+                      + '       drop foreign key ' + fk['name'] + ';\n\n'
+
+        self.__write_file(file_name, contents)
+
+
+    def __write_drop_indexes_sql(self):
+        file_name = '25-indexes.sql'
+
+        self.__notice(file_name, 1)
+
+        contents = ''
+        for index in self.__indexes_to_drop:
+            contents += 'alter table ' + index['table_name'] + "\n" \
+                      + '       drop index ' + index['index_name'] + ";\n\n"
+
+        self.__write_file(file_name, contents)
+
+
+    def __write_drop_ref_tables_sql(self):
+        file_name = '45-ref_tables.sql'
+
+        self.__notice(file_name, 1)
+
+        contents = ''
+        for name in self.__ref_tables_to_drop:
+            contents += 'drop table if exists ' + name + ';\n\n'
+
+        self.__write_file(file_name, contents)
+
+
+    def __write_drop_tables_sql(self):
+        file_name = '40-tables.sql'
+
+        self.__notice(file_name, 1)
+
+        contents = ''
+        for name in self.__tables_to_drop:
+            contents += 'drop table if exists ' + name + ';\n\n'
+
+        self.__write_file(file_name, contents)
+
+
+    def __write_drop_unique_key_constraint_sql(self):
+        file_name = '20-unique_keys.sql'
+
+        self.__notice(file_name, 1)
+
+        contents = ''
+        for uk in self.__unique_keys_to_drop:
+            contents += 'alter table ' + uk['table_name'] + "\n" \
+                      + '       drop key ' + uk['name'] + ";\n\n"
+
+        self.__write_file(file_name, contents)
+
+
+    def __write_ref_table_data_sql(self):
+        file_name = '50-ref_data.sql'
+
+        self.__notice(file_name, 1)
+
+        contents = ''
+        for data in self.__ref_data_to_add:
+            contents += ('insert into ' + data[0] + "\n"
+                      +  '(\n'
+                      +  '    id,\n'
+                      +  '    value,\n'
+                      +  '    display_order\n'
+                      +  ')\n'
+                      +  'values\n'
+                      +  '(\n'
+                      +  '    ' + str(data[1]) + ',\n'
+                      +  "    '" + re.sub("'", "''", data[2]) + "',\n"
+                      +  '    ' + str(data[3]) + ',\n'
+                      +  ');\n'
+                      +  'commit;\n\n')
+
+        for data in self.__ref_data_to_modify:
+            contents += ('update ' + data[0] + '\n'
+                      +  "   set value = '" + data[2] + "',\n"
+                      +  "       display_order = " + data[3] + "\n"
+                      +  " where id = " + data[1] + ";\n"
+                      +  "commit;\n\n")
+
+        for data in self.__ref_data_to_remove:
+            contents += 'delete from ' + data[0] + '\n' \
+                      + '      where id = ' + data[1] + ';\n' \
+                      + 'commit;\n\n'
+
+        self.__write_file(file_name, contents)
+
+
+    def __write_file(self, file_name, contents):
+        f = open(file_name, 'w')
+        f.write(contents)
+        f.close()
+
+
+    def __write_upgrade_scripts(self):
+        if not self.__enable_write_upgrade_scripts:
+            return
+
+        self.__notice('Writing upgrade scripts into current directory...')
+
+        self.__write_add_ref_tables_sql()
+        self.__write_drop_foreign_key_constraint_sql()
+        self.__write_drop_unique_key_constraint_sql()
+        self.__write_drop_indexes_sql()
+        self.__write_add_tables_sql()
+        self.__write_add_modify_columns_sql()
+        self.__write_drop_tables_sql()
+        self.__write_drop_ref_tables_sql()
+        self.__write_ref_table_data_sql()
+        self.__write_add_foreign_key_constraint_sql()
+        self.__write_add_unique_key_constraint_sql()
+        self.__write_add_indexes_sql()

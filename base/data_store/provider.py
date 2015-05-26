ba233a5275c8913bab8e7e863f94791eb0db59d5
Michael Montero <mcmontero@gmail.com>
2015-05-26 21:13:48 +0100

    Tuning how often stats get written.

--- base/data_store/provider.py
+++ base/data_store/provider.py
@@ -544,7 +544,7 @@ class DataStoreProvider(object):
 
             if Context.env_unit_test() is False and \
                Context.env_cli() is False and \
-               random.randint(1, 100) == 1:
+               random.randint(1, 500) == 1:
                 log_file = ConfigManager.value('app log file')
                 if log_file is not None:
                     requests = _thread_local_data.dsh.requests

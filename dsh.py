76cc2500e23742dbe02cd932156df8d26c9c0a5b
Michael Montero <mcmontero@gmail.com>
2016-02-08 15:21:24 +0000

    Revert "Introduced a noop provider for handling operations when a data store connection is not established."
    This reverts commit 0b2e0f54feb0a836f82b27889ffd457aafda0489.

--- dsh.py
+++ dsh.py
@@ -4,7 +4,6 @@ __author__ = 'Michael Montero <mcmontero@gmail.com>'
 
 # ----- Imports ---------------------------------------------------------------
 
-from tinyAPI.base.data_store.provider import DataStoreNOOP
 from tinyAPI.base.data_store.provider import DataStoreProvider
 
 import tinyAPI
@@ -12,6 +11,25 @@ import tinyAPI
 __all__ = [
     'dsh'
 ]
+
+# ----- Private Classes -------------------------------------------------------
+
+class NoOpDSH(object):
+    '''
+    The use of this object in __DSH is ambiguous.  It's unclear why a call
+    to a commit or rollback command would be executed without a connection
+    ever being established.
+    '''
+
+    def close(self):
+        pass
+
+    def commit(self, ignore_exceptions=True):
+        pass
+
+
+    def rollback(self, ignore_exceptions=True):
+        pass
 
 # ----- Instructions ----------------------------------------------------------
 
@@ -22,10 +40,7 @@ class __DSH(object):
 
 
     def __call__(self):
-        return \
-            (self.__provider
-                if self.__provider is not None else
-             DataStoreNOOP())
+        return self.__provider if self.__provider is not None else NoOpDSH()
 
 
     def select_db(self, connection, db, persistent=True):

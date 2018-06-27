67b9a2d47e97c1a79257e2413d7ec7de6da60387
Michael Montero <mcmontero@gmail.com>
2018-06-27 19:48:16 +0000

    Case insensitive parsing of select and show.

--- base/data_store/provider.py
+++ base/data_store/provider.py
@@ -508,7 +508,8 @@ class DataStoreMySQL(RDBMSBase):
         self.connect()
 
         is_select = False
-        if re.match('^\(?select ', sql) or re.match('^show ', sql):
+        if re.match('^\(?select ', sql, re.IGNORECASE) or \
+           re.match('^show ', sql, re.IGNORECASE):
             is_select = True
 
         cursor = self.__get_cursor()
